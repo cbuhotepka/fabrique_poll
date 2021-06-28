a44999c41fbc7efb215e0367d5acd3cba1246ebb
cbuhotepka <67138623+cbuhotepka@users.noreply.github.com>
2021-06-28 22:02:54 +0300

    Collected static files Set DEBUG = False

--- fabrique_poll/urls.py
+++ fabrique_poll/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
+from django.urls.conf import re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
 
